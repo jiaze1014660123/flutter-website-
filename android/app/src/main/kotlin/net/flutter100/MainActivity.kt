90e32e97ea45703ad218fe001541c2b341bb7c19
贾泽 <jiaze@jiazedeMacBook-Pro.local>
2020-06-12 15:15:31 +0800

    init

---
+++ android/app/src/main/kotlin/net/flutter100/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.flutter100
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:Center(
+        child: RaisedButton(
+          child: Text('测试数据接口'),
+          onPressed:()async{
+          String url = 'http://172.16.120.219:8080?actions=getProducts';
+          var res = await http.get(url);
+          String body = res.body;
+          var json = jsonDecode(body);
+          print(json);
+        }),
+      )
+    );
+  }
+}
